882f87d2fe5a342dd160dd0aff60e2d577e3af7a
JinsuBae2 <bjs7898@naver.com>
2026-06-27 16:11:21 +0900

    과정 1 - 1

--- src/course1/HelloBiodome01.java
+++ src/course1/HelloBiodome01.java
@@ -3,15 +3,15 @@ package course1;
 public class HelloBiodome01 {
     static String name;
     public static void main(String[] args) {
-        if (args.length != 0) {
+        if (args.length != 0 && !args[0].isBlank()) {
 
             // 1, 정규식 활용
-            name = args[0];
+            name = String.join(" ", args);
 
             String regex = "^(.{10}).*";
             String s = name.replaceAll(regex, "$1");
 
-            System.out.println(s);
+            System.out.println("안녕하세요, \"" + s + "\"님");
 
 
             // 2. char 변환 활용
@@ -23,6 +23,8 @@ public class HelloBiodome01 {
 //            } catch (StringIndexOutOfBoundsException s) {
 //                System.out.println("안녕하세요, \"" + name + "\"님" );
 //            }
+        } else {
+            System.out.println("이름을 입력해 주세요");
         }
     }
 }

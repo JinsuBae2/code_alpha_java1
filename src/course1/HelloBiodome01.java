7ef657c93c4db72ca1501be99221fce4f5ff3169
JinsuBae2 <bjs7898@naver.com>
2026-06-27 16:21:20 +0900

    과정 1 - 1

--- src/course1/HelloBiodome01.java
+++ src/course1/HelloBiodome01.java
@@ -3,28 +3,36 @@ package course1;
 public class HelloBiodome01 {
     static String name;
     public static void main(String[] args) {
-        if (args.length != 0 && !args[0].isBlank()) {
+        if (args.length == 0) {
+            System.out.println("이름을 입력해 주세요");
+            return;
+        }
 
-            // 1, 정규식 활용
-            name = String.join(" ", args);
+        // 1, 정규식 활용
+        name = String.join(" ", args).trim();
 
-            String regex = "^(.{10}).*";
-            String s = name.replaceAll(regex, "$1");
+        name = name.replaceAll("\\s+", " ");
 
-            System.out.println("안녕하세요, \"" + s + "\"님");
-
-
-            // 2. char 변환 활용
-//            char[] charArray = name.toCharArray();
-
-//            try {
-//                String s = String.valueOf(charArray, 0, 10);
-//                System.out.println("안녕하세요, \"" + s + "\"님" );
-//            } catch (StringIndexOutOfBoundsException s) {
-//                System.out.println("안녕하세요, \"" + name + "\"님" );
-//            }
-        } else {
+        if (name.isEmpty()) {
             System.out.println("이름을 입력해 주세요");
         }
+
+
+        String regex = "^(.{10}).*";
+        String s = name.replaceAll(regex, "$1");
+
+        System.out.println("안녕하세요, \"" + s + "\"님");
+
+
+        // 2. char 변환 활용
+//        char[] charArray = name.toCharArray();
+//
+//        try {
+//            String s = String.valueOf(charArray, 0, 10);
+//            System.out.println("안녕하세요, \"" + s + "\"님" );
+//        } catch (StringIndexOutOfBoundsException s) {
+//            System.out.println("안녕하세요, \"" + name + "\"님" );
+//        }
+
     }
 }
